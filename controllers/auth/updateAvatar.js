2e288cbdda5ac176de63c79a8a4f99455c3ad290
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-02-23 12:44:26 +0100

    fixing image name

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,9 +1,9 @@
 const { User } = require('../../models'); 
 const path = require('path'); 
 const fs = require('fs/promises'); 
-const Jimp = require('jimp'); 
-const createError = require('http-errors'); 
-
+const Jimp = require('jimp');
+const { v4: uuidv4 } = require('uuid');
+const createError = require('http-errors');
 const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars'); 
 
 
@@ -14,10 +14,10 @@ const updateAvatar = async (req, res) => {
         throw createError(400, "Avatar is required");
     }
     const { path: tempUpload, originalname } = file; 
-    const imageName = `${_id}_${originalname}`; 
+    const imageName = `${_id}_${uuidv4()}${originalname}`; 
 
 try {
-    const resultUpload = path.join(avatarsDir, originalname); 
+    const resultUpload = path.join(avatarsDir, imageName); 
     await Jimp.read(tempUpload)
         .then((avatar) => {
             return avatar.resize(250, 250).write(resultUpload);
@@ -27,7 +27,7 @@ try {
         }); 
     await fs.rename(tempUpload, resultUpload); 
     const avatarURL = path.join('avatars', imageName); 
-    await User.findByIdAndUpdate(_id, { avatarURL }); 
+    await User.findByIdAndUpdate(_id, { avatarURL }, {new: true}); 
     res.json({avatarURL}); 
 } catch (error) {
     await fs.unlink(tempUpload);
