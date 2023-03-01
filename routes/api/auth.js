82fcca4b6ff1654136691bba176d7bf31704eafb
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-03-01 13:02:17 +0100

    adding email verification

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const { validation, authenticate, ctrlWrapper, upload } = require('../../middlewares'); 
 const { auth: ctrl } = require('../../controllers');
-const { authSchema, updateSubscriptionSchema } = require('../../models');
+const { authSchema, updateSubscriptionSchema, emailSchema } = require('../../models');
 
 const router = express.Router(); 
 
@@ -10,12 +10,16 @@ router.post('/register', validation(authSchema), ctrlWrapper(ctrl.register));
 
 router.post('/login', validation(authSchema), ctrlWrapper(ctrl.login)); 
 
-router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent)); 
+router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.post('/logout', authenticate, ctrlWrapper(ctrl.logout)); 
 
 router.patch('/', authenticate, validation(updateSubscriptionSchema), ctrlWrapper(ctrl.updateSubscription));
 
 router.patch('/avatars', authenticate, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar)); 
+
+router.get('/verify/:verificationToken', ctrlWrapper(ctrl.verifyEmail)); 
+
+router.post('/verify', validation(emailSchema), ctrlWrapper(ctrl.resendVerifyEmail)); 
 
 module.exports = router; 
