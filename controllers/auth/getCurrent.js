be612e2b5bf00658495f50f9704f010303a5ffb7
ValeriiaM23 <mezencevavalerie@gmail.com>
2023-06-30 18:22:34 +0200

    adding name to get current response

--- controllers/auth/getCurrent.js
+++ controllers/auth/getCurrent.js
@@ -1,5 +1,5 @@
 const getCurrent = async (req, res) => {
-    const { email, subscription, avatarURL } = req.user;
+    const { email, subscription, avatarURL, name } = req.user;
     res.json({
         status: "success",
         code: 200,
@@ -7,6 +7,7 @@ const getCurrent = async (req, res) => {
             email,
             subscription,
             avatarURL,
+            name,
         },
     });
 
